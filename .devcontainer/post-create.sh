94bf452c518a00165790bbe78a0f35167a5a5221
Kevin Hester <kevinh@geeksville.com>
2025-07-25 20:04:29 +0000

    update ROCm lib to 6.4.2

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -12,8 +12,14 @@ sudo chown -R $USER ~/.local
 echo "Installing python requirements"
 pip3 install --user -r requirements.txt
 
+echo "Installing utilities for developers"
+pip3 install --user python-appimage setuptools wheel cx_freeze==8.3.0
+
+wget -O bin/linuxdeploy https://github.com/linuxdeploy/linuxdeploy/releases/download/1-alpha-20250213-2/linuxdeploy-x86_64.AppImage
+chmod a+x bin/linuxdeploy 
+
 echo "Installing onnxruntime-gpu for NVIDIA GPU support"
 pip install --user onnxruntime-gpu
 
 echo "Installing onnxruntime-rocm for AMD GPU support"
-pip3 install --user onnxruntime-rocm -f https://repo.radeon.com/rocm/manylinux/rocm-rel-6.4.1/
+pip3 install --user onnxruntime-rocm -f https://repo.radeon.com/rocm/manylinux/rocm-rel-6.4.2/
