e70117821a84ac7e1293d943307c4e97423b1405
Kevin Hester <kevinh@geeksville.com>
2025-09-06 12:41:26 -0700

    Change to the modern python build system (so that... I can make appimage builds and possibly even pypi based distribution)
    linux exe builds almost work again.
    
    To build a python package build:
    
    python -m build
    
    To build exe builds:
    
    python setup.py build_exe
    python setup.py bdist_msi
    python setup.py bdist_rpm

--- setup.py
+++ setup.py
@@ -1,58 +1,107 @@
 import os
 import sys
+import re
+import setuptools
+
+# Read the contents of your requirements.txt file
+with open("requirements.txt", "r", encoding="utf-8") as f:
+    install_requires = f.read().splitlines()
+
+# Read the contents of your README file for the long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 import astropy
-from cx_Freeze import Executable, setup
 
-from graxpert.version import release, version
+def get_version_info():
+    """Reads version and release from graxpert/version.py - since we now build in an isolated env."""
+    version_file_path = os.path.join("graxpert", "version.py")
+    with open(version_file_path, "r", encoding="utf-8") as f:
+        version_file_content = f.read()
 
-sys.setrecursionlimit(15_000)
+    version_match = re.search(r"^version\s*=\s*['\"]([^'\"]*)['\"]", version_file_content, re.M)
+    release_match = re.search(r"^release\s*=\s*['\"]([^'\"]*)['\"]", version_file_content, re.M)
 
-astropy_path = os.path.dirname(os.path.abspath(astropy.__file__))
+    if version_match and release_match:
+        return version_match.group(1), release_match.group(1)
+    
+    raise RuntimeError("Unable to find version and release strings in graxpert/version.py.")
 
-directory_table = [("ProgramMenuFolder", "TARGETDIR", "."), ("GraXpert", "ProgramMenuFolder", "GraXpert")]
+version, release = get_version_info()
 
-msi_data = {
-    "Directory": directory_table,
-    "ProgId": [("Prog.Id", None, None, "GraXpert is an astronomical image processing program for extracting and removing gradients in the background of your astrophotos", "IconId", None)],
-    "Icon": [("IconId", "./img/Icon.ico")],
+# Shared build options for setuptools.setup and cx_Freeze.setup
+setup_options = {
+    'name':"graxpert",
+    'version':version,
+    'description':"GraXpert is an astronomical image processing program for extracting and removing gradients in the background of your astrophotos",
+    'license':"GLP-3.0",
+
+    # A long description that will be displayed on PyPI
+    'long_description':long_description,
+    'long_description_content_type':"text/markdown",
+
+    # Find all packages automatically
+    'packages': setuptools.find_packages(),
+
+    # The dependencies that are required for the package to run
+    'install_requires': install_requires
 }
 
-msi_summary_data = {"author": "GraXpert Development Team", "comments": "<info@graxpert.com>"}
+# import cx_Freeze only when needed ---
+cx_freeze_commands = {'build_exe', 'bdist_msi', 'bdist_rpm'}
+if cx_freeze_commands.intersection(sys.argv):
+    import cx_Freeze
 
-bdist_msi_options = {
-    "add_to_path": True,
-    "data": msi_data,
-    "summary_data": msi_summary_data,
-    "upgrade_code": "{d0ba2b1d-e18e-42c9-9ded-beb9cadad494}",
-    "target_name": "GraXpert",
-    "install_icon": "./img/Icon.ico",
-}
+    sys.setrecursionlimit(15_000)
 
-bidst_rpm_options = {"release": release, "vendor": "GraXpert Development Team <info@graxpert.com>", "group": "Unspecified"}
+    astropy_path = os.path.dirname(os.path.abspath(astropy.__file__))
 
-build_options = {
-    "includes": ["astropy.constants.codata2018", "astropy.constants.iau2015", "imageio.plugins.pillow", "skimage.draw.draw", "skimage.exposure.exposure", "skimage.filters._gaussian"],
-    "include_files": [
-        ["./img", "./lib/img"],
-        ["./graxpert-dark-blue.json", "./lib/graxpert-dark-blue.json"],
-        ["./locales/", "./lib/locales/"],
-        [os.path.join(astropy_path, "units", "format", "generic_parsetab.py"), "./lib/astropy/units/format/generic_parsetab.py"],
-        [os.path.join(astropy_path, "units", "format", "generic_lextab.py"), "./lib/astropy/units/format/generic_lextab.py"],
-    ],
-    "excludes": [],
-    "include_msvcr": True,
-}
+    directory_table = [("ProgramMenuFolder", "TARGETDIR", "."), ("GraXpert", "ProgramMenuFolder", "GraXpert")]
 
-base = "Win32GUI" if sys.platform == "win32" else None
+    msi_data = {
+        "Directory": directory_table,
+        "ProgId": [("Prog.Id", None, None, "GraXpert is an astronomical image processing program for extracting and removing gradients in the background of your astrophotos", "IconId", None)],
+        "Icon": [("IconId", "./img/Icon.ico")],
+    }
 
-executables = [Executable("./graxpert/main.py", base=base, icon="./img/Icon.ico", target_name="GraXpert", shortcut_name="GraXpert {}".format(version), shortcut_dir="GraXpert")]
+    msi_summary_data = {"author": "GraXpert Development Team", "comments": "<info@graxpert.com>"}
 
-setup(
-    name="GraXpert",
-    version=version,
-    description="GraXpert is an astronomical image processing program for extracting and removing gradients in the background of your astrophotos",
-    executables=executables,
-    options={"build_exe": build_options, "bdist_msi": bdist_msi_options, "bdist_rpm": bidst_rpm_options},
-    license="GLP-3.0",
-)
+    bdist_msi_options = {
+        "add_to_path": True,
+        "data": msi_data,
+        "summary_data": msi_summary_data,
+        "upgrade_code": "{d0ba2b1d-e18e-42c9-9ded-beb9cadad494}",
+        "target_name": "GraXpert",
+        "install_icon": "./img/Icon.ico",
+    }
+
+    bidst_rpm_options = {"release": release, "vendor": "GraXpert Development Team <info@graxpert.com>", "group": "Unspecified"}
+
+    build_options = {
+        "includes": ["astropy.constants.codata2018", "astropy.constants.iau2015", "imageio.plugins.pillow", "skimage.draw.draw", "skimage.exposure.exposure", "skimage.filters._gaussian"],
+        "include_files": [
+            ["./img", "./lib/img"],
+            ["./graxpert-dark-blue.json", "./lib/graxpert-dark-blue.json"],
+            ["./locales/", "./lib/locales/"],
+            [os.path.join(astropy_path, "units", "format", "generic_parsetab.py"), "./lib/astropy/units/format/generic_parsetab.py"],
+            [os.path.join(astropy_path, "units", "format", "generic_lextab.py"), "./lib/astropy/units/format/generic_lextab.py"],
+        ],
+        "excludes": [],
+        "include_msvcr": True,
+    }
+
+    base = "Win32GUI" if sys.platform == "win32" else None
+
+    executables = [cx_Freeze.Executable("./graxpert/main.py", base=base, icon="./img/Icon.ico", target_name="GraXpert", shortcut_name="GraXpert {}".format(version), shortcut_dir="GraXpert")]
+
+    # Add the cx_Freeze options to the setup arguments dictionary
+    setup_options['executables'] = executables
+    setup_options['options'] = {
+        "build_exe": build_options,
+        "bdist_msi": bdist_msi_options,
+        "bdist_rpm": bidst_rpm_options
+    }
+
+    cx_Freeze.setup(**setup_options)
+else:
+    setuptools.setup(**setup_options)
