7e84b522cdb15a10873c9ccfca732b814eb08fea
Kevin Hester <kevinh@geeksville.com>
2025-09-11 21:40:16 +0000

    Add GPU benchmark results - 200x speedup

--- benchmark.sh
+++ benchmark.sh
@@ -1,10 +1,13 @@
 set -e
 
 USEGPU=true
+GRAXPERT="python graxpert/main.py"
+
+export PYTHONPATH=.
 
 echo "Running background extraction..."
-time graxpert -cmd background-extraction -output /tmp/testout_bk -gpu $USEGPU tests/test_images/real_crummy.fits
+time $GRAXPERT -cmd background-extraction -output /tmp/testout_bk -gpu $USEGPU tests/test_images/real_crummy.fits
 echo "Running deconvolution (object)..."
-time graxpert -cmd deconv-obj -output /tmp/testout_deconv -gpu $USEGPU /tmp/testout_bk.fits
+time $GRAXPERT -cmd deconv-obj -output /tmp/testout_deconv -gpu $USEGPU /tmp/testout_bk.fits
 echo "Running denoising..."
-time graxpert -cmd denoising -output /tmp/testout_denoise -gpu $USEGPU /tmp/testout_deconv.fits
+time $GRAXPERT -cmd denoising -output /tmp/testout_denoise -gpu $USEGPU /tmp/testout_deconv.fits
